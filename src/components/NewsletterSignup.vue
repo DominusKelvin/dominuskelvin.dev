fb528a66b569b7640177a82f3779a52f64ab9d4c
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-17 12:11:34 +0100

    feat: add a newsletter component

---
+++ src/components/NewsletterSignup.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+<template>
+  <h2 class="mt-8 text-center text-2xl font-bold text-green">Newsletter</h2>
+  <iframe
+    src="https://bbojs.substack.com/embed"
+    width="480"
+    height="320"
+    class="w-full"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
+</template>
