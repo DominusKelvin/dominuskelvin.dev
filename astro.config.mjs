da86f109ef37ba18f009d257daa79eb1e1431b22
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-03-28 23:32:57 +0100

    feat: move to using browser image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
-import image from '@astrojs/image'
 
 import vue from '@astrojs/vue'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dominuskelvin.dev',
-  integrations: [mdx(), sitemap(), tailwind(), image(), vue()],
+  integrations: [mdx(), sitemap(), tailwind(), vue()],
 })
