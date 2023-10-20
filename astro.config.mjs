63815abb9ea731e6b5752b280d3b9ab9d5a57a66
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2023-10-21 00:29:15 +0100

    feat: add redirects to important links

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,4 +9,16 @@ import vue from '@astrojs/vue'
 export default defineConfig({
   site: 'https://dominuskelvin.dev',
   integrations: [mdx(), sitemap(), tailwind(), vue()],
+  redirects: {
+    '/yt': 'https://youtube.com/@dominuskelvin',
+    '/x': 'https://x.com/Dominus_Kelvin',
+    '/nl': 'https://newsletter.dominuskelvin.dev',
+    '/gh': 'https://github.com/DominusKelvin',
+    '/in': 'https://linkedin.com/in/kelvinomereshone',
+    '/ghs': 'https://github.com/sponsors/DominusKelvin',
+    '/tkyts':
+      'https://dominuskelvin.notion.site/TKYT-sponsorship-94484c548041489eb7b181beaa287016?pvs=4',
+    '/deals':
+      'https://dominuskelvin.notion.site/Sponsorships-deals-e3270054affa4f31aa76f3de73d138ae?pvs=4',
+  },
 })
